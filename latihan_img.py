374f1df8b3500141961aceaffdf07c7602594394
ugiispoyo <ugi.ispoyowidodo@gmail.com>
2026-09-07 16:22:15 +0700

    feat: latihan 2

--- latihan_img.py
+++ latihan_img.py
@@ -6,7 +6,7 @@ img_path = os.path.join('.', 'img1.jpg')
 
 img = cv2.imread(img_path)
 
-cv2.imwrite(os.path.join('.', 'img1_output.png'), img)
+cv2.imwrite(os.path.join('.', 'img1_output.jpg'), img)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
